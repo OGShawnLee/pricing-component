72367379a4e1784b042fce7285cc56763c0aaf4e
Shawn Lee <changoloco.evg@gmail.com>
2022-05-29 09:46:50 -0500

    :wrench: chore: configure windicss

---
+++ windi.config.ts
@@ -0,0 +1,21 @@
+export default {
+  theme: {
+    colors: {
+      gradient: {
+        light: 'hsl(236, 72%, 79%)',
+        blue: 'hsl(237, 63%, 64%)',
+      },
+      slate: {
+        100: 'hsl(240, 78%, 98%)',
+        300: 'hsl(234, 14%, 74%)',
+        600: 'hsl(233, 13%, 49%)',
+        700: 'hsl(232, 13%, 33%)',
+      },
+      transparent: 'transparent',
+      white: 'hsl(0, 100%, 100%)',
+    },
+    fontFamily: {
+      montserrat: ['Montserrat'],
+    },
+  },
+};
